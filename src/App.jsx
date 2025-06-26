0a02d76b0f730dcd87e8e1236cd48be737ae537b
rob-cost <robertocostantino3@gmail.com>
2025-06-26 16:39:25 +0200

    Installed atatus

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,8 @@ function App() {
 
  
 
+ 
+
   return (
     <div className="App">
       <h1>Meet App</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as atatus from 'atatus-spa';
+atatus.config('d32ee768f990472fba4f69250596c948').install();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
